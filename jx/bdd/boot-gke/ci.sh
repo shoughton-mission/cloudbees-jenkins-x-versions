be436c4f9ae4703f22d855b7c52777a4f1cbb227
Daniel Gozalo <dgozalo@cloudbees.com>
2019-07-19 17:45:34 +0200

    removing the xunit html generator

--- jx/bdd/boot-gke/ci.sh
+++ jx/bdd/boot-gke/ci.sh
@@ -62,11 +62,9 @@ jx step bdd \
     --tests test-quickstart-spring-boot-http-gradle \
     --tests test-quickstart-golang-http \
     --tests test-quickstart-node-http \
-    --tests test-import \
-    --tests bootstrap-report \
-    --tests html-report
+    --tests test-import
 
 jx step stash \
   -c gke-boot-e2e-tests \
-  -p build/reports/junit.html \
+  -p build/reports/junit.xml \
   --bucket-url gs://daniel-dev-cluster0014-lts-3ab229c3-1b50-4cdf-b90e-e143715f1
