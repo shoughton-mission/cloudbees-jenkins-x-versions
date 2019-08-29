5a19d716b88d704560c818133204f4e289ac6a05
Daniel Gozalo <dgozalo@cloudbees.com>
2019-08-29 12:23:06 +0200

    feat: udpate versions with git patch
    feat: use the latest cloudbees-jenkins-x-config version

--- jx/bdd/boot-lh-ghe/ci.sh
+++ jx/bdd/boot-lh-ghe/ci.sh
@@ -2,10 +2,13 @@
 set -e
 set -x
 
-export GH_USERNAME="jenkins-x-bot-test"
-export GH_OWNER="cb-kubecd"
+export GH_USERNAME="dev1"
+export GH_EMAIL="jenkins-x@googlegroups.com"
+export GH_CREDS_PSW="$(jx step credential -s jx-pipeline-git-github-ghe)"
+# export GH_OWNER="cb-kubecd"
+# --git-owner $GH_OWNER
 
-export GH_CREDS_PSW="$(jx step credential -s jenkins-x-bot-test-github)"
+
 export JENKINS_CREDS_PSW="$(jx step credential -s  test-jenkins-user)"
 export GKE_SA="$(jx step credential -k bdd-credentials.json -s bdd-secret -f sa.json)"
 
@@ -31,41 +34,35 @@ echo "running the BDD tests with JX_HOME = $JX_HOME"
 # setup jx boot parameters
 export JX_VALUE_ADMINUSER_PASSWORD="$JENKINS_CREDS_PSW"
 export JX_VALUE_PIPELINEUSER_USERNAME="$GH_USERNAME"
-export JX_VALUE_PIPELINEUSER_GITHUB_USERNAME="$GH_USERNAME"
-export JX_VALUE_PIPELINEUSER_GITHUB_TOKEN="$GH_CREDS_PSW"
+export JX_VALUE_PIPELINEUSER_EMAIL="$GH_EMAIL"
 export JX_VALUE_PIPELINEUSER_TOKEN="$GH_CREDS_PSW"
 export JX_VALUE_PROW_HMACTOKEN="$GH_CREDS_PSW"
 
-# TODO temporary hack until the batch mode in jx is fixed...
+# TODO temporary hack until the batch mode in jx is fixed...
 export JX_BATCH_MODE="true"
 
-jx profile cloudbees
-
-git clone https://github.com/cloudbees/cloudbees-jenkins-x-boot-config boot-source
-cp jx/bdd/boot-gke/jx-requirements.yml boot-source
-cp jx/bdd/boot-gke/parameters.yaml boot-source/env
+git clone https://github.com/jenkins-x/jenkins-x-boot-config.git boot-source
+cp jx/bdd/boot-lh-ghe/jx-requirements.yml boot-source
+cp jx/bdd/boot-lh-ghe/parameters.yaml boot-source/env
 cd boot-source
 
+# TODO hack until we fix boot to do this too!
 helm init --client-only
 helm repo add jenkins-x https://storage.googleapis.com/chartmuseum.jenkins-x.io
 
-mkdir /workspace/source/reports
-export REPORTS_DIR=/workspace/source/reports
 
 jx step bdd \
     --use-revision \
     --version-repo-pr \
-    --versions-repo https://github.com/cloudbees/cloudbees-jenkins-x-versions.git \
-    --gopath /tmp --git-provider=github \
-    --config ../jx/bdd/boot-gke/cluster.yaml \
+    --versions-repo https://github.com/jenkins-x/jenkins-x-versions.git \
+    --config ../jx/bdd/boot-lh-ghe/cluster.yaml \
+    --gopath /tmp \
+    --git-provider=github \
+    --git-provider-url https://github.beescloud.com \
     --git-username $GH_USERNAME \
-    --git-owner $GH_OWNER \
     --git-api-token $GH_CREDS_PSW \
     --default-admin-password $JENKINS_CREDS_PSW \
     --no-delete-app \
     --no-delete-repo \
     --tests install \
-    --tests test-verify-pods \
-    --tests test-create-spring \
-    --tests test-supported-quickstarts \
-    --tests test-import
+    --tests test-create-spring
