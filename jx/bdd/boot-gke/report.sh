e7f6196108b79d3cda848daac4ff21b093b3f00e
Cosmin Cojocar <cosmin.cojocar@gmx.ch>
2019-09-25 11:02:28 +0200

    fix: change the path format of the tests reports
    The path format is now `cjxd-release-logs/reports/<date>/bdd_boot_gke_report.html`.
    
    Signed-off-by: Cosmin Cojocar <cosmin.cojocar@gmx.ch>

--- jx/bdd/boot-gke/report.sh
+++ jx/bdd/boot-gke/report.sh
@@ -2,16 +2,20 @@
 set -euo pipefail
 set -x
 
-if [ $# -ne 1 ]; then
-    echo "Please provide the report file"
+if [ $# -ne 2 ]; then
+    echo "Please provide the base dir and report file"
     exit -1
 fi
-REPORT=$1
+BASEDIR=$1
+REPORT=$2
+DATE=$(date '+%F')
 
 # activate the GCP service account before uploading the report
 gcloud auth activate-service-account --key-file $GKE_SA
 
 jx step stash \
     -c tests \
+    --basedir "${BASEDIR}" \
     -p "${REPORT}" \
     --bucket-url gs://cjxd-release-logs
+    -t "reports/${DATE}"
